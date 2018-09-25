ead23d99f477464793b8ed39a447f40bece8c773
Shams Ali <shams.ali0216@gmail.com>
2018-09-25 00:16:49 -0700

    (fix) arrays

--- controllers/lienController.js
+++ controllers/lienController.js
@@ -3,6 +3,7 @@ const pdfFiller = require("pdffiller");
 const moment = require("moment")
 
 const { assign } = Object
+const { isArray } = Array
 /**
  * lienController.js
  *
@@ -37,46 +38,58 @@ module.exports = {
         // console.log(body, 'this is body')
         // const nameRegex = null;
         // pdfFiller.generateFDFTemplate(sourcePDF, nameRegex, (err, fdfData) => err ? console.log(err) : console.log(fdfData, 'this is fdf data'));
-        const storageToDate = moment(body['P1-2-Date owner billed']).diff(moment(body['P1-2-Date Veh into possession']), 'days') * body['PER DAY']; // use dates here
-        const pdfData = assign(body,
-            {
-                "STORAGE TO DATE": storageToDate,
-                "P1-6-Storage 4000 or less": storageToDate,
-                "P1-6-Towing 4000 or less": body.TOWING,
-                'P1-6-Repairs 4000 or less': body.REPAIRS,
-                'P1-6-Parking Violations 4000 or less': body["PARKING VIOLATIONS"],
-                "P1-6-Cost of Selling 4000 or less": body["COST FOR LIEN SALE"],
-                "P1-6-Total of 2 (A-D) 4000 or less": +body.TOWING + +body.REPAIRS + +body["COST FOR LIEN SALE"] + storageToDate,
-                "P1-7-Area Code": body.tel.slice(0, 3),
-                "P1-7-Telephone No": body.tel.slice(3).split('-').join(''),
-                "P1-1-Year": body.year,
-                "P1-1-Lic Exp Date": moment(body["P1-1-Lic Exp Date"]).format("MM DD YY"), // not required leave empty
-                "P1-2-Date Veh into possession": moment(body["P1-2-Date Veh into possession"]).format("MM DD YY"),
-                "P1-2-Date owner billed": moment(body["P1-2-Date owner billed"]).format("MM DD YY"),
-                "P1-2-Date work-serv completed": moment(body["P1-2-Date work-serv completed"]).format("MM DD YY"),
-                "DATE NOTICE MAILED": moment(body["DATE NOTICE MAILED"]).format("MM DD YY"),
-                "DATE OF SALE": moment(body["DATE OF SALE"]).format("MM DD YY"),
-                "REGISTERED OWNER": (body["REGISTERED OWNER"] || []).reduce((sanitize, { name, address, city, state, zip }, index) => 
-                    sanitize.concat(`${index + 1}. ${name}\n ${address}\n ${city}, ${state} ${zip}\n`), ''),
-                "LEGAL OWNER": (body["LEGAL OWNER"] || []).reduce((sanitize, { name, address, city, state, zip }, index) => 
-                    sanitize.concat(`${index + 1}. ${name}\n ${address}\n ${city}, ${state} ${zip}\n`), ''),
-                "INTERESTED PARTIES": (body["INTERESTED PARTIES"] || []).reduce((sanitize, { name, address, city, state, zip }, index) => 
-                    sanitize.concat(`${index + 1}. ${name}\n ${address}\n ${city}, ${state} ${zip}\n`), ''),
-            },
-            [...body["REGISTERED OWNER"] || [], ...body["LEGAL OWNER"] || [], body["INTERESTED PARTIES"] || []].reduce((sanitize, { name, address, city, state, zip }, index) => (assign(sanitize, {
-                [`Name_${index + 1}`]: name,
-                [`Street_${ index + 1 }`]: address,
-                [`CityStateZipCountry_${ index + 1}`]: `${city} ${state} ${zip}`,
-            })), {})
-        );
-        pdfFiller.fillForm(sourcePDF, destinationPDF, pdfData, err => {
-            if (err) {
-                console.error(err, 'this is error in pdfiller')
-                throw err
-            };
-            res.contentType("application/pdf");
-            res.send(fs.readFileSync(destinationPDF));
-        });
+        try {
+            const storageToDate = moment(body['P1-2-Date owner billed']).diff(moment(body['P1-2-Date Veh into possession']), 'days') * body['PER DAY']; // use dates here
+            const pdfData = assign(body,
+                {
+                    "STORAGE TO DATE": storageToDate,
+                    "P1-6-Storage 4000 or less": storageToDate,
+                    "P1-6-Towing 4000 or less": body.TOWING,
+                    'P1-6-Repairs 4000 or less': body.REPAIRS,
+                    'P1-6-Parking Violations 4000 or less': body["PARKING VIOLATIONS"],
+                    "P1-6-Cost of Selling 4000 or less": body["COST FOR LIEN SALE"],
+                    "P1-6-Total of 2 (A-D) 4000 or less": +body.TOWING + +body.REPAIRS + +body["COST FOR LIEN SALE"] + storageToDate,
+                    "P1-7-Area Code": body.tel.slice(0, 3),
+                    "P1-7-Telephone No": body.tel.slice(3).split('-').join(''),
+                    "P1-1-Year": body.year,
+                    "P1-1-Lic Exp Date": moment(body["P1-1-Lic Exp Date"]).format("MM DD YY"), // not required leave empty
+                    "P1-2-Date Veh into possession": moment(body["P1-2-Date Veh into possession"]).format("MM DD YY"),
+                    "P1-2-Date owner billed": moment(body["P1-2-Date owner billed"]).format("MM DD YY"),
+                    "P1-2-Date work-serv completed": moment(body["P1-2-Date work-serv completed"]).format("MM DD YY"),
+                    "DATE NOTICE MAILED": moment(body["DATE NOTICE MAILED"]).format("MM DD YY"),
+                    "DATE OF SALE": moment(body["DATE OF SALE"]).format("MM DD YY"),
+                    "REGISTERED OWNER": (body["REGISTERED OWNER"] ? body["REGISTERED OWNER"] : []).reduce((sanitize, { name, address, city, state, zip }, index) => 
+                        sanitize.concat(`${index + 1}. ${name}\n ${address}\n ${city}, ${state} ${zip}\n`), ''),
+                    "LEGAL OWNER": (body["LEGAL OWNER"] ? body["LEGAL OWNER"] : []).reduce((sanitize, { name, address, city, state, zip }, index) => 
+                        sanitize.concat(`${index + 1}. ${name}\n ${address}\n ${city}, ${state} ${zip}\n`), ''),
+                    "INTERESTED PARTIES": (body["INTERESTED PARTIES"] ? body["INTERESTED PARTIES"] : []).reduce((sanitize, { name, address, city, state, zip }, index) => 
+                        sanitize.concat(`${index + 1}. ${name}\n ${address}\n ${city}, ${state} ${zip}\n`), ''),
+                },
+                [].concat(
+                    isArray(body["REGISTERED OWNER"]) ? body["REGISTERED OWNER"] : [], 
+                    isArray(body["LEGAL OWNER"]) ? body["LEGAL OWNER"] : [],
+                    isArray(body["INTERESTED PARTIES"]) ? body["INTERESTED PARTIES"] : [],
+                ).reduce((sanitize, { name, address, city, state, zip }, index) => (assign(sanitize, {
+                    [`Name_${index + 1}`]: name,
+                    [`Street_${ index + 1 }`]: address,
+                    [`CityStateZipCountry_${ index + 1}`]: `${city} ${state} ${zip}`,
+                })), {})
+            );
+            pdfFiller.fillForm(sourcePDF, destinationPDF, pdfData, err => {
+                if (err) {
+                    console.error(err, 'this is error in pdfiller')
+                    throw err
+                };
+                res.contentType("application/pdf");
+                res.send(fs.readFileSync(destinationPDF));
+            });
+        }
+        catch(err){
+            console.error(err, 'this is error');
+            res.send(err)
+        }
+        
+        
 
 
     },
